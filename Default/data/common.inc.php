38a1f043e5c9fe8c283992f4491403b7a5e00946
w8800385 <543460485@qq.com>
2020-02-02 14:55:07 +0800

    初始化织梦

---
+++ Default/data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = '219.133.59.44';
+$cfg_dbname = 'anranbxcom';
+$cfg_dbuser = 'anranbxcom';
+$cfg_dbpwd = 'pv4w9y2km9';
+$cfg_dbprefix = 'de';
+$cfg_db_language = 'utf8';
+
+
+?>
